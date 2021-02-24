b08f37f298e4946fb32cead2ec5f23c8c2513dc5
JD Wilson <jdwilson@qcsoftware.com>
2021-02-24 12:17:05 -0500

    Was pulling the wrong data. Fixed that

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# The "secrets" file contains the bearer_token value. This will not be added to the public repo
+# The "secrets" file contains the bearer_token value. You will need to supply your own if you wish to run this locally
 from secrets import *
 from flask import Flask, json, request
 import requests
@@ -6,29 +6,36 @@ import requests
 app = Flask(__name__)
 
 
-def get_response(business_id):
-    uri = 'https://api.yelp.com/v3/businesses/' + business_id + '/reviews'
-    return requests.get(uri, headers={'Authorization': 'Bearer ' + bearer_token})
+def get_location(uri):
+    business_data = requests.get(uri, headers={'Authorization': 'Bearer ' + bearer_token}).json()
+    return business_data["location"]["display_address"]
 
 
-def get_reviews(response):
-    response_data = response.json()
+def get_reviews(uri):
+    reviews = requests.get(uri + "/reviews", headers={'Authorization': 'Bearer ' + bearer_token}).json()
+    return reviews["reviews"]
+
+
+def get_review_details(location, reviews):
     data = []
-    for review in response_data["reviews"]:
-        data.append({"review_id": review["id"],
-                     "review_text": review["text"],
-                     "user_id": review["user"]["id"],
-                     "profile_url": review["user"]["profile_url"]})
+    for review in reviews:
+        data.append({"user_name": review["user"]["name"],
+                     "avatar_image_url": review["user"]["image_url"],
+                     "location": location,
+                     "rating": review["rating"],
+                     "review_content": review["text"]})
     return data
 
 
 @app.route("/reviews", methods=["GET"])
 def main():
     try:
-        restaurant = request.args.get("id")
-        response = get_response(restaurant)
-        restaurant_data = get_reviews(response)
-        return json.dumps(restaurant_data, sort_keys=True, indent=4)
+        restaurant_id = request.args.get("id")
+        uri = 'https://api.yelp.com/v3/businesses/' + restaurant_id
+        location = get_location(uri)
+        reviews = get_reviews(uri)
+        restaurant_data = get_review_details(location, reviews)
+        return json.dumps(restaurant_data, sort_keys=False, indent=4)
     except KeyError:
         return "Restaurant not found"
 
